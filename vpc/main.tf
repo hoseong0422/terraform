32c688c2577c7bcee6f872f19e76e8b60de3702c
Hoseong <hoseong0422@gmail.com>
2023-11-29 23:05:15 +0900

    Terraform init commit

---
+++ vpc/main.tf
@@ -0,0 +1,51 @@
+resource "aws_vpc" "hs_terraform_test_vpc" {
+  cidr_block = "10.0.0.0/16"  
+  tags = {
+    Name = "hs_terraform_test_vpc"
+    Terraform = "true"
+  }
+}
+
+resource "aws_subnet" "hs_terraform_test_public_2a" {
+  vpc_id     = aws_vpc.hs_terraform_test_vpc.id
+  cidr_block = "10.0.0.0/20"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "hs_terraform_test_public_2a"
+    Terraform = "true"
+  }
+}
+
+resource "aws_subnet" "hs_terraform_test_public_2c" {
+  vpc_id     = aws_vpc.hs_terraform_test_vpc.id
+  cidr_block = "10.0.16.0/20"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "hs_terraform_test_public_2c"
+    Terraform = "true"
+  }
+}
+
+resource "aws_subnet" "hs_terraform_test_private_2a" {
+  vpc_id     = aws_vpc.hs_terraform_test_vpc.id
+  cidr_block = "10.0.128.0/20"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "hs_terraform_test_private_2a"
+    Terraform = "true"
+  }
+}
+
+resource "aws_subnet" "hs_terraform_test_private_2c" {
+  vpc_id     = aws_vpc.hs_terraform_test_vpc.id
+  cidr_block = "10.0.144.0/20"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "hs_terraform_test_private_2c"
+    Terraform = "true"
+  }
+}
